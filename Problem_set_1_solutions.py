cba9f9cdb4ced0cf46f64f76f9c0ae01ed542726
Roye Fanka <46478464+FankaRoy@users.noreply.github.com>
2023-04-25 03:06:53 +0100

    Update Problem_set_1_solutions.py

--- Problem_set_1_solutions.py
+++ Problem_set_1_solutions.py
@@ -1,27 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-One way of transporting cows is to always pick the heaviest cow that will fit onto the spaceship first. This is an example of a greedy algorithm. So if there are only 2 tons of free space on your spaceship, with one cow that's 3 tons and another that's 1 ton, the 1 ton cow will get put onto the spaceship.
-
-Implement a greedy algorithm for transporting the cows back across space in the function greedy_cow_transport. The function returns a list of lists, where each inner list represents a trip and contains the names of cows taken on that trip.
-
-Note: Make sure not to mutate the dictionary of cows that is passed in!
-
-Assumptions:
-
-The order of the list of trips does not matter. That is, [[1,2],[3,4]] and [[3,4],[1,2]] are considered equivalent lists of trips.
-All the cows are between 0 and 100 tons in weight.
-All the cows have unique names.
-If multiple cows weigh the same amount, break ties arbitrarily.
-The spaceship has a cargo weight limit (in tons), which is passed into the function as a parameter.
-Example:
-
-Suppose the spaceship has a weight limit of 10 tons and the set of cows to transport is {"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}.
-
-The greedy algorithm will first pick Jesse as the heaviest cow for the first trip. There is still space for 4 tons on the trip. Since Maggie will not fit on this trip, the greedy algorithm picks Maybel, the heaviest cow that will still fit. Now there is only 1 ton of space left, and none of the cows can fit in that space, so the first trip is [Jesse, Maybel].
-
-For the second trip, the greedy algorithm first picks Maggie as the heaviest remaining cow, and then picks Callie as the last cow. Since they will both fit, this makes the second trip [[Maggie], [Callie]].
-
-The final result then is [["Jesse", "Maybel"], ["Maggie", "Callie"]].
+Student: Roye Fanka
+Year: 2020
 """
 
 # Enter your code for the Greedy Cow Transport here 
@@ -65,6 +45,58 @@ def greedy_cow_transport(cows, limit=10):
 greedy_cow_transport({'Polaris': 20, 'Milkshake': 75, 'Muscles': 65, 'Louis': 45, 'Clover': 5, 'Patches': 60, 'Lotus': 10, 'Horns': 50, 'Miss Bella': 15, 'MooMoo': 85}, 100)
 greedy_cow_transport({'Rose': 50, 'Dottie': 85, 'Willow': 35, 'Coco': 10, 'Betsy': 65, 'Patches': 12, 'Lilly': 24, 'Buttercup': 72, 'Abby': 38, 'Daisy': 50}, 100)
 greedy_cow_transport({'Rose': 42, 'Starlight': 54, 'Willow': 59, 'Coco': 59, 'Betsy': 39, 'Luna': 41, 'Buttercup': 11, 'Abby': 28}, 120)
+
+# Problem 2
+def brute_force_cow_transport(cows,limit=10):
+    """
+    Finds the allocation of cows that minimizes the number of spaceship trips
+    via brute force.  The brute force algorithm should follow the following method:
+    1. Enumerate all possible ways that the cows can be divided into separate trips
+    2. Select the allocation that minimizes the number of trips without making any trip
+        that does not obey the weight limitation
+            
+    Does not mutate the given dictionary of cows.
+    Parameters:
+    cows - a dictionary of name (string), weight (int) pairs
+    limit - weight limit of the spaceship (an int)
+    
+    Returns:
+    A list of lists, with each inner list containing the names of cows
+    transported on a particular trip and the overall list containing all the
+    trips
+    """
+    # TODO: Your code here
+    # intialize final list of trips
+    trips = []
+    # create power list using helper function, and sort it - shortest first!
+    power_list = sorted(get_partitions(cows), key = len)
+    # Note that this returns a list of names (strings), and we will need to do
+    # dictionary lookup later
+    # Now time to filter the power list:
+    possibilities = []
+    for i in power_list:
+        ship = []
+        for j in i:
+            ship_weights = []
+            for k in j:
+                ship_weights.append(cows[k])
+                #print(ship_weights)
+            ship.append(sum(ship_weights))
+            #print(ship)
+        if all(d <= limit for d in ship):
+            possibilities.append(i)
+    # possibiliies now contains some duplicates, which need to be removed
+    pruned_possibilities = []
+    for k in possibilities:
+        if k not in pruned_possibilities:
+            pruned_possibilities.append(k)
+    # now find the minimum list length:
+    min_list_len = min(map(len, pruned_possibilities))
+    for l in pruned_possibilities:
+        if len(l) == min_list_len:
+            return l
+        
+
 
 
 
